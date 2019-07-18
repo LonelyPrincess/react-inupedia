2cb5fec718b1338f54779db18a07fb7379292f87
Sara <sara.her.su@gmail.com>
2019-07-18 16:58:30 +0100

    refactor: Organize source code in subfolders

--- src/main/App.js
+++ src/main/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../res/images/logo.svg';
+import '../res/styles/App.css';
 
 function App() {
   return (
