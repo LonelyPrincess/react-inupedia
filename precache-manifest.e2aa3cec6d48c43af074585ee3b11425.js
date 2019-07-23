d2ed1219d6e7a312b91723eb76a73efb2308b207
Sara <sara.her.su@gmail.com>
2019-07-23 21:49:36 +0100

    Updates

---
+++ precache-manifest.e2aa3cec6d48c43af074585ee3b11425.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86dca292f8ceebf13bd596f1e5f865e",
+    "url": "/react-inupedia/index.html"
+  },
+  {
+    "revision": "af8263d6745e769aa611",
+    "url": "/react-inupedia/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0ef95d39e649cf0a02a1",
+    "url": "/react-inupedia/static/css/main.6d0bd223.chunk.css"
+  },
+  {
+    "revision": "af8263d6745e769aa611",
+    "url": "/react-inupedia/static/js/2.7579b88f.chunk.js"
+  },
+  {
+    "revision": "0ef95d39e649cf0a02a1",
+    "url": "/react-inupedia/static/js/main.b25a85b3.chunk.js"
+  },
+  {
+    "revision": "b29af63fb3de6180ac1b",
+    "url": "/react-inupedia/static/js/runtime~main.90be6720.js"
+  },
+  {
+    "revision": "7f319a6d87faf6c73cf64c6c89a72be1",
+    "url": "/react-inupedia/static/media/bone.7f319a6d.svg"
+  },
+  {
+    "revision": "e14d1fe7a9a4b67a43b633f091917be7",
+    "url": "/react-inupedia/static/media/dog-house.e14d1fe7.svg"
+  },
+  {
+    "revision": "d31860a59b6a84f897221bd74657aada",
+    "url": "/react-inupedia/static/media/shiba.d31860a5.svg"
+  }
+]);
